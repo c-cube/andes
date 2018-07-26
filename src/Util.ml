027f9a81baaac413f275db9f7a358d4007d57f30
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-07-26 14:51:35 -0500

    perf: inline logf stub

--- src/Util.ml
+++ src/Util.ml
@@ -29,15 +29,14 @@ end = struct
 
   let enable l = lvl_ := l
 
-  let logf lvl k =
-    if lvl <= !lvl_ then (
-      k (fun fmt ->
-        let out = Format.std_formatter in
-        Format.fprintf out "@[<2>@{<Blue>[andes %d|%.3f]@}@ " lvl (Sys.time());
-        Format.kfprintf
-          (fun out -> Format.fprintf out "@]@.")
-          out fmt)
-    )
+  let logf_real lvl k =
+    k (fun fmt ->
+      let out = Format.std_formatter in
+      Format.fprintf out "@[<2>@{<Blue>[andes %d|%.3f]@}@ " lvl (Sys.time());
+      Format.kfprintf
+        (fun out -> Format.fprintf out "@]@.")
+        out fmt)
 
+  let[@inline] logf lvl k = if lvl <= !lvl_ then logf_real lvl k
   let[@inline] log lvl s = logf lvl (fun k->k "%s" s)
 end
